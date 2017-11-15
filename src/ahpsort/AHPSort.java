1bcb7000732d2402137ab680cef75de48cf9c50d
Andrea Serrano Urea <0andreasu@gmail.com>
2017-11-15 23:39:03 +0100

    Loading file's interface created.
    Also changed currentDataFile type from String to File.

--- src/ahpsort/AHPSort.java
+++ src/ahpsort/AHPSort.java
@@ -5,6 +5,8 @@
  */
 package ahpsort;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 /**
@@ -13,12 +15,12 @@ import java.util.Scanner;
  */
 public class AHPSort {
     
-    static String currentDataFile = null;
+    static File currentDataFile = null;
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int opt = 0;
         
         do {
@@ -26,15 +28,15 @@ public class AHPSort {
             opt = showMainMenu();
             
             switch (opt){
-                case 1:
+                case 1: //Solve class problems
                     //TODO
                     System.out.println("Lo haría, pero aún no me han implementado.\n");
                     break;
-                case 2:
+                case 2: //Load custom file
                     //TODO
-                    System.out.println("Lo haría, pero aún no me han implementado.\n");
+                    showLoadingFileMenu();
                     break;
-                case 3:
+                case 3: //Solve using custom file's data
                     if (currentDataFile == null){
                         opt = -1;
                         break;
@@ -61,7 +63,7 @@ public class AHPSort {
             System.out.println("1. Resolver ejercicio de clase (no necesita cargar fichero)");
             System.out.print("2. Cargar fichero de datos");
 
-            if(currentDataFile != null) System.out.println(" (Archivo actual: " + currentDataFile);
+            if(currentDataFile != null) System.out.println(" (Archivo actual: " + currentDataFile.getName() + ")");
             else System.out.println(" (No hay un archivo cargado)");
 
             if (currentDataFile != null) System.out.println("3. Resolver usando datos cargados.");
@@ -81,6 +83,45 @@ public class AHPSort {
         }while(option < 0 || option > 3);
         
         return option;
+    }
+    
+    /**
+     * It shows the menu for loading a custom file. 
+     * 
+     * It handles the possible errors which can appear. If a file is stored,
+     * it must exist.
+     */
+    private static void showLoadingFileMenu() throws IOException{
+        System.out.println("---Cargar fichero---");
+        System.out.println("Instrucciones para crear un archivo de valores.");
+        System.out.println("-Primera línea: Número de criterios.");
+        System.out.println("-Segunda línea: Número de alternativas.");
+        System.out.println("-Tercera línea: Línea en blanco");
+        System.out.println("-Cuarta línea: Matriz de criterios, separando los valores con comas e indicando el fin de una línea con un guión. \nEjemplo de matriz identidad 2x2: 1,0-0,1");
+        System.out.println("-Sucesivas líneas(sin líneas en blanco): Matrices de comparaciones según criterio.");
+        System.out.println("-Para ver un ejemplo, consulta el archivo sample_input.txt");
+        
+        Scanner keyboard = new Scanner(System.in);
+        File file = null;
+        boolean exit = true;
+        do {
+            System.out.print("Nombre del archivo (escribe 0 para volver atrás): ");
+            file = new File(keyboard.next());
+            
+            if (!file.getName().equals("0")){
+                if (!file.exists()){
+                    System.out.println("El archivo no existe.");
+                    exit = false;
+                } else {
+                    currentDataFile = file;
+                    exit = true;
+                }
+            } else {
+                exit = true;
+            }
+            
+        } while(!exit);
+
     }
     
 }
